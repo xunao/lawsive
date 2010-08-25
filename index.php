f0e81b63d940f3e6dd7fb5ed17a7723dd9f201f1
=375865795 <=375865795.com>
2010-08-25 16:14:10 +0800

    index

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                                             </div>
                                             <div id="view">
                                                         <div class="c_title">
-                                                                   <div class="c_t_n" ><font>新闻·分析</font><div class="c_t_b" style="width:210px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div>
+                                                                   <div class="c_t_n" ><font>新闻·分析</font><div class="c_t_b" style="width:200px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div>
                                                          </div>
                                                          <div class="v_c_b">
 			                                                                   <font class="v_c_t_1">案例： 中国沦为发展基地</font><font color="red" class="l_add">[律氏]</font>
@@ -101,7 +101,7 @@
                                             </div>
 			                                <div id="change">
 			                                                          <div class="c_title">
-                                                                                         <div class="c_t_n" ><font>观点·专栏</font><div class="c_t_b" style="width:70px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div>
+                                                                                         <div class="c_t_n" ><font>观点·专栏</font><div class="c_t_b" style="width:65px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div>
                                                                       </div>
                                                                       <div class="change_b">
 									                                                           <div class="change_t" >
@@ -130,7 +130,7 @@
 						                                                 </div>
 						                    </div>
 						                    <div id="lecture">
-						                                                   <div class="c_title"><div class="c_t_n" ><font>在线调查</font><div class="c_t_b" style="width:266px;"></div></div></div>
+						                                                   <div class="c_title"><div class="c_t_n" ><font>在线调查</font><div class="c_t_b" style="width:260px;"></div></div></div>
 						                                                   <div id="s_l" >
 						                                                                       <font>―北大教授力挺郭德纲，赞其是民族英雄，你认为？</font>
 					                                                                           <form action = "http://www.blabla.cn/asdocs/html_tutorials/choose.asp" method = "post">
@@ -153,13 +153,13 @@
 		                                        </div>
 		                                        <div class="talk">
 		                                                        <div class="talk_t">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
 		                                                        </div>
 		                                                         <div class="talk_t" style="float:right">
-		                                                                          <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
+		                                                                          <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
 		                                                                         <div class="talk_c"><img src="/images/index/talk_p.jpg"><div class="talk1">邵子力</div><div class="talk2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div></div>
@@ -167,13 +167,13 @@
 		                                        </div>
 		                                        <div class="ylawyer">
 		                                                         <div class="ylaw">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                         </div>
 		                                                         <div class="ylaw" style="float:right">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
@@ -181,13 +181,13 @@
 		                                         </div>
 		                                           <div class="ylawyer">
 		                                                         <div class="ylaw">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                         </div>
 		                                                         <div class="ylaw" style="float:right">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
@@ -195,13 +195,13 @@
 		                                         </div>
 		                                           <div class="ylawyer">
 		                                                         <div class="ylaw">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>高端访谈</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div></div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                         </div>
 		                                                         <div class="ylaw" style="float:right">
-		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:145px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
+		                                                                         <div class="c_title"><div class="c_t_n" ><font>青年律师</font><div class="c_t_b" style="width:150px;"></div><img src="/images/index/c_t_right.jpg"><img src="/images/index/c_t_left.jpg"></div> </div>
 		                                                                         <div class="ylaw1">邵子力</div><img src="/images/index/lawyer_p.jpg">
 		                                                                         <div class="ylaw2">经济开发健康减肥卡斯放就尽快康复了 金额近阿卡软件阿卡软件俄既然人家加入</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
 		                                                                         <div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div><div class="c_p_r_r"><font class="point">·</font>侵犯知识产权罪中“非法经营数额”</div>
