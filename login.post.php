80b64b944c03bfadd27e147ce3f2bcd0823e9231
=375865795 <=375865795.com>
2010-08-30 14:32:35 +0800

    login

--- login.post.php
+++ login.post.php
@@ -4,8 +4,10 @@ use_jquery_ui();
 js_include_tag('login');
 $login_name =$_POST['login_name'];
 $password =$_POST['password'];
+$expire =$_POST['time'];
+if($expire=NULL){$expire=0;}else{$expire=7;}
 $member = new member();
-$record = $member->login($login_name,$password,0);
+$record = $member->login($login_name,$password,$expire);
 $user = $record[0];
 ?>
 <div id="t_l_r">
