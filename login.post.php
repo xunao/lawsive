54e4a280f81d60fc399672f325f73a399999f968
=375865795 <=375865795.com>
2010-08-30 17:14:09 +0800

    login

--- login.post.php
+++ login.post.php
@@ -3,7 +3,7 @@ include_once './frame.php';
 $login_name =$_POST['login_name'];
 $password =$_POST['password'];
 $expire =$_POST['time'];
-if($expire=NULL){$expire=0;}else{$expire=7;}
+if($expire=NULL){$expire=0;}else{$expire=7;}/*登录时选择记住登录状态默认保存帐号密码在COOKIE里7天*/
 $member = new member();
 $record = $member->login($login_name,$password,$expire);
 $user = $record[0];
