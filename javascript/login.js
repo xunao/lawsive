fc702811af1978219036e80388e806100cb2ceb7
pingguo <070144451.cjj@163.com>
2010-08-31 09:50:39 +0800

    cc

--- javascript/login.js
+++ javascript/login.js
@@ -33,7 +33,42 @@ $(function(){
 			$('#b_u_u').html(data);
 		});
 	});
+	$("#login_btn2").click(function(){
+		alert('no');
+		var login_name = $("#name2").val();
+		var password = $("#password2").val();
+		
+		if(login_name == '' ){
+			alert('请输入用户名');
+			return false;
+		}
+		if(password ==''){
+			alert('请输入密码');
+			return false;
+		}
+		$.post("_ajax.login.post.php",{"login_name":login_name,"password":password},function(data){
+			$('#t_l_r').html(data);
+			$('#b_u_u').html(data);
+		});
+	});
+	
 	$('.logout').click(function(){
 			window.location.href="/logout.php/";
 	});
+	document.onkeydown = function(e){ 
+		  var ev = document.all ? window.event : e;
+		  if(ev.keyCode==13 || ev.ctrlKey) {
+		   	if($("#name0").val()==''){
+			alert('请输入用户名');
+				return false;
+			}
+			if($("#password0").val()==''){
+				alert('请输入密码');
+				return false;
+			}
+			$("#login_btn0").click();
+		  }
+	}
 });
+
+	 
--- login.post.php
+++ login.post.php
@@ -4,8 +4,13 @@ $login_name =$_POST['login_name'];
 $password =$_POST['password'];
 $expire =$_POST['time'];
 if($expire=NULL){$expire=0;}else{$expire=7;}
-$member = new member();
-$record = $member->login($login_name,$password,$expire);
-$user = $record[0];
-redirect("/")
+$user = member::login($login_name,$password,$expire);
+
+if($user == null){
+	alert(您的帐号或密码输入有误！);
+	redirect("/login.php/");
+}else{
+	redirect("/");
+}
+
 ?>
