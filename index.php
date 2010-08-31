41ae3952527a4c661a88442c4568fff7bd9c4847
robbin <robbin@robbin-laptop.(none)>
2010-08-31 21:51:29 +0800

    robbin

--- index.php
+++ index.php
@@ -119,59 +119,7 @@
 		                                </div>
 		                         </div>
                        </div>
-                       <div id="center_r" >
-                              <div id="expert">
-		                            <div id="exp_h"><div ><a href="">热点案例</a></div><div ><a href="">专家库</a></div><div><a href="">专业指南</a></div></div>  
-		                            <div id="exp_c"><a href=""><img src="/images/index/alb.jpg" border="0"></a><a href=""><img src="/images/index/alb.jpg" border="0"></a><a href=""><img src="/images/index/alb.jpg" border="0"></a></div>
-		                            <div id="exp_d"><font>律师事务所</font><font>律师事务所</font><font>律师事务所</font></div>
-		                      </div>
-		                      <div id="rss"><form action="" method="post"><div id="rss_in"><img src="/images/index/a.jpg"><input type="text" name="email" value="输入您的接收邮件"><a href=""><img id="rss_p" src="images/index/rss_q.jpg" border="0"></a></div></form></div>
-                              <div id="meeting">
-                                     <div id="buss_h"><font>会议·讲座</font><a href=""><font  style="float:right; margin-right:5px; letter-spacing:0.1px; font-size:11px; font-weight:200;">更多>></font></a></div>
-                                     <div id="buss_t"><div class="buss_b1" id="buss_t1">日期</div><div class="buss_b1" id="buss_t2">主题</div><div class="buss_b1" id="buss_t1">状态</div> </div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2"><a href="">律师事务所与市场营销·上海</a></div><div class="buss_b2" id="buss_t1"><a href="">报名</a></div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2"><a href="">律师事务所与市场营销·上海</a></div><div class="buss_b2" id="buss_t1">结束</div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2"><a href="">律师事务所与市场营销·上海</a></div><div class="buss_b2" id="buss_t1">结束</div></div>      
-                              </div>
-                              <div id="bussiness">
-                                     <div id="buss_h"><font>最新交易</font><a href=""><font  style="float:right; margin-right:5px; letter-spacing:0.1px; font-size:11px; font-weight:200;">更多>></font></a></div>
-                                     <div id="buss_t"><div class="buss_b1" id="buss_t1">时间</div><div class="buss_b1" id="buss_t2">案件</div><div class="buss_b1" id="buss_t1">金额</div> </div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>
-                              </div>
-                              <div id="job">
-                                     <div id="buss_h"><font>推荐职位</font><a href=""><font  style="float:right; margin-right:5px; letter-spacing:0.1px; font-size:11px; font-weight:200;">更多>></font></a></div>
-                                     <div id="buss_t"><div class="buss_b1" id="buss_t1">时间</div><div class="buss_b1" id="buss_t2">案件</div><div class="buss_b1" id="buss_t1">金额</div> </div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>
-                                     <div id="buss_t"><div class="buss_b2" id="buss_t1">10/07/12</div><div class="buss_b2" id="buss_t2">模仿他人企业名称的侵权行为</div><div class="buss_b2" id="buss_t1">30000</div></div>      
-                              </div>
-                              <div id="cr_ad"><img src="/images/index/cr_ad1.jpg"><img src="/images/index/cr_ad2.jpg"></div>
-                              <div id="lawyer" class="border">
-                                     <div id="law_h"><div ><a href="" >律师排行</a></div><div><a href="">律所排行</a></div><div><a href="">专业排行</a></div><div><a href="">最佳雇主</a></div><a href="" style="float:right">更多>></a></div>
-                                     <div id="law_tt" >
-                                           <div class="lawyer1"><div class="lawyer_c" style="margin-top:18px;">1</div><img alt="" src="/images/index/lawyer_i.jpg"><div class="lawyer_t">某某某</div><div class="lawyer_s">紫金矿山回复离开房间封口机看风景看</div></div>
-                                           <div class="lawyer2"><div class="lawyer_c" style="margin-top:2px;">2</div><div class="lawyer_p">紫金矿山回复离开房间封口机看风景看开始接受卡</div></div>
-                                           <div class="lawyer2"><div class="lawyer_c" style="margin-top:2px;">3</div><div class="lawyer_p">紫金矿山回复离开房间封口机看风景看开始接受卡</div></div>
-                                           <div class="lawyer2"><div class="lawyer_c" style="margin-top:2px;">4</div><div class="lawyer_p">紫金矿山回复离开房间封口机看风景看开始接受卡</div></div>
-                                           <div class="lawyer2"><div class="lawyer_c" style="margin-top:2px;">5</div><div class="lawyer_p">紫金矿山回复离开房间封口机看风景看开始接受卡</div></div>
-                                           <div class="lawyer2" style="border:0px;"><div class="lawyer_c" style="margin-top:2px;">6</div><div class="lawyer_p">紫金矿山回复离开房间封口机看风景看开始接受卡</div></div>
-                                     </div>
-                              </div>
-                              <img alt="" src="/images/index/add.jpg" id="add">
-                              <div class="rank" >
-                                     <div class="r_title"><div class="on_lec_h" style="width:357px;"> <font>法律市场调查和排名</font></div></div>
-                                     <div class="block"><img class="v_i" src="/images/index/rank.jpg"><div class="v_c"><div class="v_c_t"><font class="rank1" size="5">一名资深顾问和</font><font class="rank2">8月4日，2010年客和份额解放军额外发我会计法 </font></div></div></div>
-                                     <div class="block"><img class="v_i" src="/images/index/rank.jpg"><div class="v_c"><div class="v_c_t"><font class="rank1" size="5">一名资深顾问和</font><font class="rank2">8月4日，2010年客和份额解放军额外发我会计法 </font></div></div></div>
-                              </div>
-                              <div class="rank" style="border:0">
-                                     <div class="r_title"><div class="on_lec_h" style="width:357px;"><font> 法律市场调查和排名</font></div></div>
-                                     <div class="block"><img class="v_i" src="/images/index/rank.jpg"><div class="v_c"><div class="v_c_t"><font class="rank1" size="5">一名资深顾问和</font><font class="rank2">8月4日，2010年客和份额解放军额外发我会计法 </font></div></div></div>
-                                     <div class="block"><img class="v_i" src="/images/index/rank.jpg"><div class="v_c"><div class="v_c_t"><font class="rank1" size="5">一名资深顾问和</font><font class="rank2">8月4日，2010年客和份额解放军额外发我会计法 </font></div></div></div>
-                              </div>
-                              <img alt="" src="/images/index/add.jpg" id="add">
-                       </div>
+                       <?php include_once(dirname(__FILE__).'/inc/right.php'); ?> 
                        <div id="cl_ad"><a href=""><img src="/images/index/cl_ad.jpg" border=0></a></div>
              </div>      
             <?php include_once(dirname(__FILE__).'/inc/bottom.php'); ?>       
---
+++ trading.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>律氏中文网</title>
+<meta name="keywords" content="律氏" />
+	<meta name="description" content="律氏" />
+<?php	
+		include ('./frame.php');
+		use_jquery_ui();
+		css_include_tag('public','trading');
+		js_include_tag('login');
+		$user = member::current();
+  	?>
+<body>
+      <div id="ibody">
+              <?php include_once(dirname(__FILE__).'/inc/top.php'); ?>
+              <div id="center">
+              	<div id="center_l">
+	              	<div id="new_trade">
+	              		<div class="c_title"><div class="c_t_n" ><font>最新交易</font><div class="c_t_b" style="width:280px;"></div></div></div>
+	              		<div id=title>
+	              			<div id=time>时间</div>
+	              			<div id=case>案件</div>
+	              			<div id=money>金额</div>
+	              		</div>
+	              		<?php for($i=0;$i<6;$i++){ ?>
+	              		<div class=content>
+	              			<div class=time>2010年5月</div>
+	              			<div class=case> 外资所 IPO<br>金石律师事务所  <span>王晓晓</span> 律师</div>
+	              			<div class=money>2000万</div>
+	              		</div>
+	              		<?php } ?>
+	              		<div class=content style="border:none;">
+	              			<div class=time>2010年5月</div>
+	              			<div class=case> 外资所 IPO<br>金石律师事务所  <span>王晓晓</span> 律师</div>
+	              			<div class=money>2000万</div>
+	              			<div id=more><a href="">更多>></a></div>
+	              		</div>
+	              	 </div>
+	              	 <div id=trade_ranking>
+	              	 	<div class="c_title"><div class="c_t_n" ><font>表现最佳律所</font><div class="c_t_b" style="width:95px;"></div></div></div>
+	             	 	<div id=title>
+	             	 		<select class="select1"></select>
+	             	 		<select class="select5"></select>
+	             	 	</div>
+	             	 	<div class="content_title">
+	             	 		<div class=title_l>已完成（根据金额）</div>
+	             	 		<div class=title_r><a href=""><img border=0 src="/images/trade/ranking_title_icon.gif"></a></div>
+	             	 	</div>
+	             	 	<div class="context_title">
+	             	 		<div class="rank">排序</div>
+	             	 		<div class="law">律所</div>
+	             	 		<div class="change">金额</div>
+	             	 	</div>
+	             	 	<?php for($i=1;$i<6;$i++){ ?>
+	             	 	<div class="context">
+	             	 		<div class="rank"><?php echo $i; ?></div>
+	             	 		<div class="law">上海东方律师事务所</div>
+	             	 		<div class="change">30000</div>
+	             	 	</div>
+	             	 	<?php } ?>
+	             	 	<div class="content_title">
+	             	 		<div class=title_l>已完成（根据数量）</div>
+	             	 		<div class=title_r><a href=""><img border=0 src="/images/trade/ranking_title_icon.gif"></a></div>
+	             	 	</div>
+	             	 	<div class="context_title">
+	             	 		<div class="rank">排序</div>
+	             	 		<div class="law">律所</div>
+	             	 		<div class="change">数量</div>
+	             	 	</div>
+	             	 	<?php for($i=1;$i<6;$i++){ ?>
+	             	 	<div class="context">
+	             	 		<div class="rank"><?php echo $i; ?></div>
+	             	 		<div class="law">上海东方律师事务所</div>
+	             	 		<div class="change">20</div>
+	             	 	</div>
+	             	 	<?php } ?>
+	             	 	<div class="content_title">
+	             	 		<div class=title_l>已完成（根据金额）</div>
+	             	 		<div class=title_r><a href=""><img border=0 src="/images/trade/ranking_title_icon.gif"></a></div>
+	             	 	</div>
+	             	 	<div class="context_title">
+	             	 		<div class="rank">排序</div>
+	             	 		<div class="law">律所</div>
+	             	 		<div class="change">金额</div>
+	             	 	</div>
+	             	 	<?php for($i=1;$i<6;$i++){ ?>
+	             	 	<div class="context">
+	             	 		<div class="rank"><?php echo $i; ?></div>
+	             	 		<div class="law">上海东方律师事务所</div>
+	             	 		<div class="change">20</div>
+	             	 	</div>
+	             	 	<?php } ?>
+	             	 	<div class="content_title">
+	             	 		<div class=title_l>已完成（根据金额）</div>
+	             	 		<div class=title_r><a href=""><img border=0 src="/images/trade/ranking_title_icon.gif"></a></div>
+	             	 	</div>
+	             	 	<div class="context_title">
+	             	 		<div class="rank">排序</div>
+	             	 		<div class="law">律所</div>
+	             	 		<div class="change">金额</div>
+	             	 	</div>
+	             	 	<?php for($i=1;$i<6;$i++){ ?>
+	             	 	<div class="context">
+	             	 		<div class="rank"><?php echo $i; ?></div>
+	             	 		<div class="law">上海东方律师事务所</div>
+	             	 		<div class="change">20</div>
+	             	 	</div>
+	             	 	<?php } ?>
+	             	 </div>
+	              	 <div id="search_trade">
+	              	 	<div class="c_title"><div class="c_t_n" ><font>交易搜索</font><div class="c_t_b" style="width:280px;"></div></div></div>
+	              	 	<div id=content>
+	              	 		<div id=title>——请根据以下条件输入相关关键字进行搜索</div>
+	              	 		类型<select class="select1"></select>　　时　　间<select class="select2"></select>
+	              	 		<div class=space></div>
+	              	 		金额<select class="select1"></select>　　适用法律<select class="select2"></select>
+	              	 		<div class=space></div>
+	              	 		<select class="select3"></select><select class="select4"></select>
+	              	 		<div class=space></div>
+	              	 		<select class="select3"></select><br>
+	              	 		<button>搜索</button>
+	              	 	</div>
+	              	 </div>
+	              	 <div id="trade_image">
+	              	 	<a href=""><img src="/images/trade/image1.jpg"></a>
+	              	 </div>
+	              	 
+	             </div>
+              	 <?php include_once(dirname(__FILE__).'/inc/right.php'); ?>
+              </div>     
+            <?php include_once(dirname(__FILE__).'/inc/bottom.php'); ?>       
+      </div>
+</body>
+</html>
