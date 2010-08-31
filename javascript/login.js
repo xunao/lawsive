f24d3ed4dfc5b463cd64d3985d67119c5aeee390
pingguo <070144451.cjj@163.com>
2010-08-31 15:26:30 +0800

    login.js change

--- javascript/login.js
+++ javascript/login.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-$(function(){
-	$("#login_btn0").click(function(){
-		var login_name = $("#name0").val();
-		var password = $("#password0").val();
-		
-		if(login_name == '' ){
-			alert('请输入用户名');
-			return false;
-=======
 function checkInput(login_name,password){
 		u = login_name.val();
 		a = password.val();
 		if(login_name != null){
 			if(!u ||u.length == 0){alert('请输入用户名！'); login_name.focus(); return false;}
 			if(u.length >48||u.length <6){alert('请您正确的输入用户名！'); return false;}
->>>>>>> e64b1fb6db2a738f93bae73d8e7ae4b19ad9cc38
 		}
 		if(password != null){
 			if(!a||a.length == 0){alert('请输入你的密码！'); password.focus(); return false;}
@@ -61,9 +50,6 @@ $(function(){
 	$('.logout').click(function(){
 			window.location.href="/logout.php/";
 	});
-<<<<<<< HEAD
-});
-=======
 	
 	document.onkeydown = function(e){ 
 		  var ev = document.all ? window.event : e;
@@ -71,12 +57,6 @@ $(function(){
 			$("#login_btn0").click();
 		  }
 	};
-//	document.onkeydown = function(e){ 
-//		  var ev = document.all ? window.event : e;
-//		  if(ev.keyCode==13 || ev.ctrlKey) {
-//			$("#login_btn0").click();
-//		  }
-//	};
 	document.onkeydown = function(e){ 
 		  var ev = document.all ? window.event : e;
 		  if(ev.keyCode==13 || ev.ctrlKey) {
@@ -86,4 +66,3 @@ $(function(){
 });
 
 	 
->>>>>>> e64b1fb6db2a738f93bae73d8e7ae4b19ad9cc38
