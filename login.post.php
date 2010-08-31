c9c3416cda9b4e4ad8fd273bc8c3a4f369424757
=375865795 <=375865795.com>
2010-08-31 13:36:23 +0800

    login

--- login.post.php
+++ login.post.php
@@ -1,11 +1,13 @@
 <?php
 include_once './frame.php';
+use_jquery_ui();
+js_include_tag('login');
 $login_name =$_POST['email'];
 $password =$_POST['password'];
 $expire =$_POST['time'];
-if($expire=NULL){$expire=0;}else{$expire=7;}/*登录时选择记住登录状态默认保存帐号密码在COOKIE里7天*/
+if($expire==NULL){$expire=0;}else{$expire=7;}/*登录时选择记住登录状态默认保存帐号密码在COOKIE里7天*/
 $member = new member();
-$record = $member->login($login_name,$password,$expire);
+$record = $member->login($login_name,$password,0);
 $user = $record[0];
 redirect("/")
 ?>
