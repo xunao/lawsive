b6a332e2f1321fcc088a539856bc445bb18a1d34
pingguo <070144451.cjj@163.com>
2010-09-02 11:09:03 +0800

    cc

--- coverperson.php
+++ coverperson.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>HallOfFame</title>
+<title>coverperson</title>
 <meta name="keywords" content="律氏" />
 	<meta name="coverperson" content="律氏" />
 <?php	
