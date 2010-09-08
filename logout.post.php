b0ae2dc2e6acefaf9e79300cb18493a4588bb41c
sauger <sauger@163.com>
2010-09-08 16:35:25 +0800

    delete unuse files

--- logout.post.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "../frame.php";
-	unset($_SESSION['admin_user_name']);
-	unset($_SESSION['admin_user_id']);
-	unset($_SESSION['admin_nick_name']);
-	unset($_SESSION['role_name']);
-	unset($_SESSION['role_level']);
-	redirect('/login.php');
-?>
