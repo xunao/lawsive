21bf8656f23c9a249440457216222f56c7b6e745
=375865795 <=375865795.com>
2010-09-09 13:25:00 +0800

    s2

--- news/index.php
+++ news/index.php
@@ -10,6 +10,19 @@
 		css_include_tag('public','news/index');
 		js_include_tag('login');
 		$user = member::current();
+		$db=get_db();
+		//$news_id=$_POST('news_id');
+		$news_id=1;
+		$record=$db->query("select * from lawsive.news where id='{$news_id}'");
+		$record_f=explode(' ', $record[0]->related_news);
+		$latest_test=$db->query("select * from lawsive.news where author='{$record[0]->author}' and id!='{$news_id}' ");
+		$record_f_t=array();
+		$record_f_d=array();
+		for ($i = 0; $i < 3; $i++) {
+			$sql=$db->query("select * from lawsive.news where id='{$record_f[$i]}'");
+			$record_f_t[]=$sql[0]->title;
+			$record_f_d[]=$sql[0]->created_at;
+		}
   	?>
 <body>
       <div id="ibody">
@@ -17,41 +30,20 @@
           <div id="center">
           	<div id="left">
           		<div id="left_title">
-          			<div id="l_t1">2010年09月07日 &nbsp 15:32 &nbsp PM</div>
-          			<div id="l_t2">中国工业企业“西进”</div>
-          			<div id="l_t3">作者：英国《金融时报》&nbsp <a href=""><font color="#086CC4">彼得•马什</font></a></div>
+          			<div id="l_t1"><?php echo $record[0]->created_at;?></div>
+          			<div id="l_t2"><?php echo $record[0]->title;?></div>
+          			<div id="l_t3">作者：&nbsp <a href=""><font color="#086CC4"><?php echo $record[0]->author;?></font></a></div>
           			<div id="l_t4">字号 &nbsp &nbsp &nbsp &nbsp &nbsp 背景 </div><div id="l_t5">收藏 电邮 打印 评论<a href=""><font color="#9E2F2F">[1条]</font></a>&nbsp &nbsp<a href=""><font color="#9E2F2F">英文</font></a>&nbsp &nbsp<a href=""><font color="#9E2F2F">对照</font></a></div>
           		</div>
           		<div id="content">
-          			<div class="cont">在英国兰开夏郡(Lancashire)罗奇代尔市(Rochdale)一座巨穴般的工厂里，一群工人正在对一批高科技机床进行最后加工，这些机床用于为美军使用的重炮制造关键部件。半小时车程之外，西约克郡(West Yorkshire)哈利法克斯市(Halifax)的另一家工厂里，另一个工程小组正在组装用于石油、筑路和航天行业的大型机械。</div>
-          			<div class="cont">这两家工厂原先分属英国最著名的两家机床制造商，如今，两家工厂的180名工人都在为重庆市国有资产监督管理委员会下属的庞大商业帝国效力。重庆国资委是中国最大的国有控股企业之一。</div>
-          			<div class="cont">今年4月，重庆机电股份有限公司(Chongqing Machinery & Electric Company)以2000万英镑 (合3100万美元)收购了总部位于英国的、主要从事机床生产的Precision Technologies Group（简称PTG）。工业全球化模式正在发生变化，这宗交易成为其最新例证。</div>
-          			<div class="cont">重庆机电是重庆市国资委下属30家国有重点企业之一。英国磐华咨询(Pancathay Consulting)董事总经理罗曼蒂(Madeleine Sturrock)表示，尽管迄今为止，中国企业仅收购了少量欧美工业企业，但此类交易可能会变得更为常见。</div>
-          			<div id="cont_i">
-          				<a href=""><img alt="" src="/images/news/lenovo_ad.jpg" border="0"></a>
-          				<font class="cont_i_f">“虽然中国已成为极其重要的工业国家，但许多中国企业意识到，自己必须向西方学习（技术和管理技巧），”罗曼蒂表示。</font>
-          				<font class="cont_i_f"> 在PTG的案例中，中方计划在未来数年投入1233镑，主要投向罗奇代尔，以增强该公司在机床技术领域的实力。</font>
-          				<font class="cont_i_f"> 重庆机电是中国最大的机床制造商之一，去年机床销售额约为3亿美元，但在技术上，该公司没有许国外机械制造商那么先进。</font>
-          				<font>“我的中国同事们知道，以现有技术水平，他们在中国生产的机床，在精细程度上无法与我们在PTG英国工厂生产的机床相比，”PTG首席执行官托尼?班南(Tony Bannan)表示。</font>
-          			</div>
-          			<div class="cont">PTG并不是中国买家瞄准的唯一英国企业。去年，羊绒供应商宁夏中银绒业股份有限公司(Ningxia Zhong-yin Cashmere Company)收购了邓肯公司(Todd & Duncan)，后者是一家专门生产成品羊绒纱线的企业。</div>
-          			<div class="cont">邓肯公司董事总经理马嘉顺(Ian McGrattan)表示，新中国东家的计划包括“强调（公司的）苏格兰背景”。邓肯公司去年的销售额约为2000万英镑，拥有200名员工。</div>
-          			<div class="cont">“我们的业务内容是向欧洲各地共900家专业纺织品企业销售少量的高品质羊绒纱线，这些企业通常要求在下单后几天、有时甚至是几小时内收到货物。（新东家）意识到如果他们试图在中国现有基地进行生产，将无法运营此项业务，”马嘉顺表示。</div>
-          			<div class="cont">第三笔类似模式的收购发生在3年前，当时中国大型国有化工企业中国蓝星(China National Blue-star)收购了格里姆斯比的一家工厂，该工厂原本是化工企业Courtaulds旗下的业务。</div>
-          			<div class="cont">此后，中国蓝星花费了2000万英镑，对生产设施进行升级，从而具备了生产碳纤维的能力，这是一种用于飞机制造等领域的高科技材料。</div>
-          			<div class="cont">与此同时，蓝星还利用其英国子公司的专业技能，在中国兰州工业园区建设了一座大型碳纤维工厂。美国金融集团百仕通(Blackstone)目前持有蓝星20%的股权。</div>
-          			<div class="cont">由于碳纤维与一系列工程行业休戚相关，中国方面认为，碳纤维生产对于未来50年非常重要。</div>
-                    <div class="cont">蓝星英国子公司董事总经理戴维?瑟韦斯(David Service)表示，尽管获取碳纤维生产技术是此笔收购交易背后的主要原因之一，但蓝星集团也有意利用格里姆斯比的生产基地，在西方市场拓展业务。</div>
-          			<div class="cont">“收购工厂之后公司还可以采取其它举措，例如收购碳纤维技术领域的其它欧洲企业，”瑟韦斯表示。</div>
-          			<div class="cont">“十年前，（涉及中国企业）的此类交易纯粹是为了获取技术知识，但如今，另一个同样重要的目的，是为中国企业提供更多西方视角。”</div>
-          			<div class="cont">译者/何黎</div>
-          			<div class="cont_b1">彼得•马什上一篇文章：</div>
-          			<div class="cont_b2"><a href="">重庆机电拟将英国子公司在伦敦上市</a> <font class="cont_b3">2010-09-07</font></div>
+          		    <div><?php echo $record[0]->content;?></div>
+          		    <div class="cont_b1"><?php echo $record[0]->author;?>上一篇文章：</div>
+          			<div class="cont_b2"><a href=""><?php echo $latest_test[0]->title;?></a> <font class="cont_b3"><?php echo date('Y-m-d',strtotime($latest_test[0]->created_at));?></font></div>
           			<div class="cont_b1">您可能感兴趣的文章：</div>
-          			<div class="cont_b2"><a href="">中国国资委应更进一步 </a><font class="cont_b3">2010-08-26</font></div>
-          			<div class="cont_b2"><a href="">中国企业进入德国</a> <font class="cont_b3">2010-08-13</font></div>
-          			<div class="cont_b2"><a href="">后危机时代中国企业面临的战略转变 </a><font class="cont_b3">2010-08-12</font></div>
-          			<div class="cont_b2"><font class="cont_b4">本文涉及话题：</font><a href="">工业</a> &nbsp <a href="">中国企业</a></div>
+          			<div class="cont_b2"><a href=""><?php echo $record_f_t[0];?></a><font class="cont_b3"><?php echo date('Y-m-d',strtotime($record_f_d[0]));?></font></div>
+          			<div class="cont_b2"><a href=""><?php echo $record_f_t[1];?></a> <font class="cont_b3"><?php echo date('Y-m-d',strtotime($record_f_d[1]));?></font></div>
+          			<div class="cont_b2"><a href=""><?php echo $record_f_t[2];?> </a><font class="cont_b3"><?php echo date('Y-m-d',strtotime($record_f_d[2]));?></font></div>
+          			<div class="cont_b2"><font class="cont_b4">本文涉及话题：</font><a href=""><?php echo $record[0]->keywords;?></a></div> 
           		</div>
           		<div id="bee_ad"><a href=""><img alt="" src="/images/news/bee_ad.jpg" border="0"></a></div>
           		<div id="comment">
@@ -105,23 +97,23 @@
                        <div id="rss"><form action="" method="post"><div id="rss_in"><img src="/images/index/a.jpg"><input type="text" name="email" value="输入您的接收邮件"><a href=""><img id="rss_p" src="/images/news/rss_q.jpg" border="0"></a></div></form></div>
                        <div class="news_view">
                        		<div class="c_title" ><div class="c_t_n" ><font color="#003869" size="2">最新观点</font><div class="c_t_b" style="width:105px;"></div></div></div>	
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
                        </div>
                        <div class="news_view" style="margin:10px 0 ; float:right">
                        		<div class="c_title" ><div class="c_t_n" ><font color="#003869" size="2">最新动态</font><div class="c_t_b" style="width:105px;"></div></div></div>	
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
                        </div>
                        <div class="news_view">
                        		<div class="c_title" ><div class="c_t_n" ><font color="#003869" size="2">会员服务</font><div class="c_t_b" style="width:105px;"></div></div></div>	
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
                        </div>
                        <div class="news_view" style="margin:10px 0 ; float:right">
                        		<div class="c_title" ><div class="c_t_n" ><font color="#003869" size="2">相关链接</font><div class="c_t_b" style="width:105px;"></div></div></div>	
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
-                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
+                       		<div class="n_v">社科部证实招考取消加分</div><div class="n_v">社科部证实招考取消加分</div><div class="n_v"><a href="">社科部证实招考取消加分</a></div><div class="n_v"><a href="">社科部证实招考取消加分</a></div>
                        </div>
                  </div>
           	</div>
