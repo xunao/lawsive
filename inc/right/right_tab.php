eac4eaf4457737fbfb097f8d1db7b709e87df310
robbin <robbin@robbin-laptop.(none)>
2010-09-10 15:16:30 +0800

    robbin

--- inc/right/right_tab.php
+++ inc/right/right_tab.php
@@ -1,8 +1,8 @@
 <style>
 	#right_tab{width:358px; margin-bottom:20px; padding-bottom:5px; background:#ECEEF4; font-size:12px;}
 	#right_tab #tab_left{width:84px; margin-top:5px; margin-left:5px;}
-	#right_tab #tab_left .tab1{width:84px; height:19px; margin-bottom:1px; text-align:center; color:#666666; background:url(/images/right/right_tab2.jpg) no-repeat; overflow:hidden;}
-	#right_tab #tab_left .tab1.select{height:20px; color:#003869; font-weight:bold; background:url(/images/right/right_tab1.jpg) no-repeat;}
+	#right_tab #tab_left .tab1{width:84px; height:19px; margin-bottom:1px; text-align:center; line-height:19px; color:#666666; background:url(/images/right/right_tab2.jpg) no-repeat; overflow:hidden;}
+	#right_tab #tab_left .tab1.select{height:20px; color:#003869; font-weight:bold; background:url(/images/right/right_tab1.jpg) no-repeat; line-height:20px;}
 	#right_tab #tab_right{width:260px; height:100px; margin-top:5px; line-height:25px; background:#C1DCEC;}
 	#right_tab #tab_right .cl{width:240px; height:25px; margin-left:10px; overflow:hidden;}
 </style>
