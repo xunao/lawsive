75ddca37bcb7964e219c429d6d603279f2781a73
pingguo <070144451.cjj@163.com>
2010-09-10 12:19:53 +0800

    cc

--- admin/member/resume_edit.php
+++ admin/member/resume_edit.php
@@ -22,9 +22,9 @@
 		$user2 = new Table('member_resume');
 		$resume = $user2->find($resume_id);
 		$db = get_db();	
-		$edu = $db->query("select * from lawsive.member_education_history where member_id = '$id'");
+		$edu = $db->query("select * from lawsive.member_education_history where member_id = '$id' order by id asc");
 		$nume = count($edu);
-		$job = $db->query("select * from lawsive.member_job_history where member_id = '$id'");
+		$job = $db->query("select * from lawsive.member_job_history where member_id = '$id' order by id asc");
 		$numj = count($job);
 		}else{
 			alert('没有简历信息！');
@@ -66,22 +66,26 @@
 		</tr>
 		<tr class=tr4>
 			<td class=td1 width=15%>教育经历</td>
-			<td><div id="edu_add" style="width:10%;"><a href="">添加教育经历</a></div></td>
+			<td><div id="edu_add" style="width:10%;"><a title="添加教育经历" href=""><img src="/images/admin/btn_add.png" border=0 /></a></div></td>
 		</tr>
 		<?php for($i = 0; $i<$nume; $i++){?>
 		<tr class=tr4>
 			<td class=td1 width=15%>学历档案<?php echo $i+1;?>#</td>
-			<td><div style="width:80%;" >从<?php echo $edu[$i]->start_date;?>到<?php echo $edu[$i]->end_date;?>在<?php echo $edu[$i]->college;?>学习</div><div class="edu_edit" name="<?php echo $edu[$i]->id;?>" value="<?php echo $i+1;?>" style="width:10%;"><a href="">编辑详情</a></div></td>
+			<td><div style="width:80%;" >从<?php echo $edu[$i]->start_date;?>到<?php echo $edu[$i]->end_date;?>在<?php echo $edu[$i]->college;?>学习</div><div class="edu_edit" name="<?php echo $edu[$i]->id;?>" value="<?php echo $i+1;?>" style="width:3%;"><a title="编辑" href=""><img src="/images/admin/btn_edit.png" border=0 /></a></div><div class="del2" type="member_education_history" name="<?php echo $edu[$i]->id;?>" style="width:3%;"><a title="删除" href=""><img src="/images/admin/btn_delete.png" border=0 /></a></div></td>
 		</tr>
 		<?php }?>
 		<tr class=tr4>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class=tr4>
 			<td class=td1 width=15%>工作经历</td>
-			<td><div id="job_add" style="width:10%;"><a href="">添加工作经历</a></div></td>
+			<td><div id="job_add" style="width:10%;"><a title="添加工作经历" href=""><img src="/images/admin/btn_add.png" border=0 /></a></div></td>
 		</tr>
 		<?php for($i = 0; $i<$numj; $i++){?>
 		<tr class=tr4>
 			<td class=td1 width=15%>工作档案<?php echo $i+1;?>#</td>
-			<td><div style="width:80%;" >从<?php echo $job[$i]->start_date;?>到<?php echo $job[$i]->end_date;?>在<?php echo $job[$i]->company;?>担任<?php echo $job[$i]->title;?></div><div class="job_edit" name="<?php echo $job[$i]->id; ?>" value="<?php echo $i+1;?>" style="width:10%;"><a href="">编辑详情</a></div></td>
+			<td><div style="width:80%;" >从<?php echo $job[$i]->start_date;?>到<?php echo $job[$i]->end_date;?>在<?php echo $job[$i]->company;?>担任<?php echo $job[$i]->title;?></div><div class="job_edit" name="<?php echo $job[$i]->id; ?>" value="<?php echo $i+1;?>" style="width:3%;"><a title="编辑" href=""><img src="/images/admin/btn_edit.png" border=0 /></a></div><div class="del2" type="member_job_history" name="<?php echo $job[$i]->id;?>" style="width:3%;"><a title="删除" href=""><img src="/images/admin/btn_delete.png" border=0 /></a></div></td>
 		</tr>
 		<?php }?>
 		<tr class=tr4>
@@ -94,7 +98,7 @@
 		</tr>
 		<tr class=tr4>
 			<td class=td1 width=15%>首次营业时间</td>
-			<td><input type="text" name="post[work_from]" value="<?php echo $resume->work_form;?>" class="required"></td>
+			<td><input id="work_from" type="text" name="post[work_form]" value="<?php echo mb_substr($resume->work_form, 0,10);?>" class="required"></td>
 		</tr>
 		<tr class=tr4>
 			<td class=td1 width=15%>从业年数</td>
