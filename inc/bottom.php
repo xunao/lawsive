34c58a1b3b41af79c25a28e1b318bb432eab78a5
sauger <sauger@163.com>
2010-09-15 14:17:28 +0800

    adjust login function

--- inc/bottom.php
+++ inc/bottom.php
@@ -1,26 +1,25 @@
- <div id="bom_up">
-                       <?php if($user == ''){?>
-                              <div id="b_u_u">
-			                                 <div class="t_l_r_c" >用户名：</div><div class="t_l_r_c"><input id="name1" type="text" name="login_name" size=10/></div>
-			                                 <div class="t_l_r_c" >密码：</div><div class="t_l_r_c"><input id="password1" type="password" name="password"/></div>
-			                                 <div class="t_l_r_c" id="login_btn1"><img src="/images/index/log_in.jpg" border=0/></div><div id="zhuce"><a href=""><font  color=#BBBBBB>注册</font></a></div>
-                              </div>
-                              <?php }else{?>
-                              <div id="b_u_u">
-								      <div class="t_l_r_c" >欢迎您，<?php $level = $user->member_level; if($level==1){echo '普通会员';}elseif($level==2){echo '二级会员';}elseif($level==3){echo '三级会员';}elseif($level==4){echo '四级会员';};?><?php if($user->name != ''){echo $user->name;}else{echo $user->login_name;} ;?></div>
-									  <div class="logout">退出登录</div>
-								</div>
-                              <?php }?>
-                       <div id="b_u_d"><a href=""><font>关于律氏中文网</font></a>-<a href=""><font>动态新闻</font></a>-<a href=""><font>广告服务</font></a>-<a href=""><font>诚聘英才</font></a>-<a href=""><font>会员活动</font></a>-<a href=""><font>隐私声明</font></a>-<a href=""><font>网站声明</font></a>-<a href=""><font>联系我们</font></a></div>
-             </div>
-             <div id="bottom">
-                     <div id="bom_down">
-                              <div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                              <div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                              <div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                              <div class="b_d_x" ></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                              <div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                              <div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
-                     </div>      
-                     <div id="bottom1">电话：010-52550331 传真：010-51411547 E-mail：editor@legalweek.cn</div><div id="bottom2">ⓒCopyright 北京新华律讯商业咨询有限公司 2008</div><div id="bottom3">网络备案许可证：京ICP备09001622号</div>
-            </div>       
+<div id="bom_up">
+	<div id="b_u_u">
+	</div>
+	<div id="b_u_d">
+		<a href=""><font>关于律氏中文网</font></a>-
+		<a href=""><font>动态新闻</font></a>-
+		<a href=""><font>广告服务</font></a>-
+		<a href=""><font>诚聘英才</font></a>-
+		<a href=""><font>会员活动</font></a>-
+		<a href=""><font>隐私声明</font></a>-
+		<a href=""><font>网站声明</font></a>-
+		<a href=""><font>联系我们</font></a>
+	</div>
+</div>
+<div id="bottom">
+	<div id="bom_down">
+		<div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+		<div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+		<div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+		<div class="b_d_x" ></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+		<div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+		<div class="b_d_x"></div><div class="b_d"><a href="" class="b_d_h"><font>[动态]</font></a><br><a href=""><font>最新案件</font></a><br><a href=""><font>律所</font></a><br><a href=""><font>律师</font></a><br><a href=""><font>专题报道</font></a><br><a href=""><font>新闻</font></a></div>
+	</div>      
+	<div id="bottom1">电话：010-52550331 传真：010-51411547 E-mail：editor@legalweek.cn</div><div id="bottom2">ⓒCopyright 北京新华律讯商业咨询有限公司 2008</div><div id="bottom3">网络备案许可证：京ICP备09001622号</div>
+</div>       
--- inc/top.php
+++ inc/top.php
@@ -1,7 +1,5 @@
 <div id="title">
-    <div id="title_ad">
-    	<a href=""><img src="/images/index/title_ad.jpg" border="0"></a>
-    </div>
+	<?php js_include_tag('jquery.cookie','front/pub');?>
 	<div id="title_log">
 	    <div id="t_l_l"></div>
 	    <div id="t_l_c">
@@ -10,29 +8,8 @@
 	        <a href=""><font>法学院</font></a><font>|</font>
 			<a href=""><font>个人服务</font></a>
 		</div>
-		<?php if($user == ''){?>
 		<div id="t_l_r">
-			<div class="t_l_r_c" >用户名：</div>
-			<div class="t_l_r_c">
-				<input id="login_name" type="text" name="login_name" size=10/>
-			</div>
-			<div class="t_l_r_c" >密码：</div>
-			<div class="t_l_r_c">
-				<input id="password" type="password" name="password"/>
-			</div>
-			<div class="t_l_r_c" id="login_btn0">
-				<img src="/images/index/log_in.jpg" border=0/>
-			</div>
-			<div id="zhuce">
-				<a href=""><font  color=#BBBBBB>注册</font></a>
-			</div>
 		</div>
-		<?php }else{?>
-		<div id="t_l_r">
-			<div class="t_l_r_c" >欢迎您，<?php $level = $user->member_level; if($level==1){echo '普通会员';}elseif($level==2){echo '二级会员';}elseif($level==3){echo '三级会员';}elseif($level==4){echo '四级会员';};?><?php if($user->name != ''){echo $user->name;}else{echo $user->login_name;} ;?></div>
-			<div class="logout">退出登录</div>
-		</div>
-		<?php }?>
 	</div>
 	<div id="title_b">
 		<div id="logo"><a href=""><img src="/images/index/logo_lawsive.jpg" border="0"></a></div>
@@ -101,57 +78,4 @@
 		<div id="head_r"><a href="">我的律氏</a></div>
 	</div>
 	<div id="cbx"><div id="center_h"></div></div>
-	<script type="text/javascript">
-		function show_submenu(index){
-			$('.nav a').removeClass('selected');
-			$('.nav a:eq('+index + ')').addClass('selected');
-			switch (index){
-				case 0:
-					$("#cbx").html("<div id='center_h'></div>");
-					break;
-				case 1:
-					$("#cbx").html("<div id='center_h'><a href='/news/lawyer/' >律所/律师</a>|<a href='/news/job/'>职位</a>|<a href='/news/topic/topic.php'>专题</a>|<a href='/view/research.php'>调研/报告</a>|<a href='#'>新法速递</a></div>");
-					break;
-				case 2:
-					$("#cbx").html("<div id='center_h' style='margin-left:24px;'><a href='/interview/halloffame.php' >人物墙</a>|<a href='/interview/cover.php'>封面人物</a>|<a href='/interview/interview.php'>高端访谈</a>|<a href='#'>律氏访谈</a></div>");
-					break;
-				case 3:
-					$("#cbx").html("<div id='center_h' style='margin-left:160px;'><a href='/hot/hot.php' >聚焦</a>|<a href='#'>事件</a>|<a href=''>辩论</a></div>");
-					break;
-				case 4:
-					$("#cbx").html("<div id='center_h' style='margin-left:145px;'><a href='/view/column.php' >特约专栏</a>|<a href='/view/analysis.php'>分析</a>|<a href='#'>评论</a>|<a href=''>书评</a>|<a href='/news/regulation.php'>法规解读</a></div>");
-					break;
-				case 5:
-					$("#cbx").html("<div id='center_h' style='margin-left:298px;'><a href='#' >律所wiki</a>|<a href='/office.php'>律所推荐</a></div>");
-					break;
-				case 6:
-					$("#cbx").html("<div id='center_h' style='margin-left:405px;'><a href='/search_news.php' >最新案例库</a></div>");
-					break;
-				case 7:
-					$("#cbx").html("<div id='center_h' style='margin-left:438px;'><a href='/news/trade/' >最新交易</a>|<a href='#'>交易库</a></div>");
-					break;
-				case 8:
-					$("#cbx").html("<div id='center_h' style='margin-left:428px;'><a href='#' >个人法务</a>|<a href='#'>企业法务</a>|<a href='/expert'>高端诊断</a>|<a href='#'>定制服务</a></div>");
-					break;
-				case 9:
-					$("#cbx").html("<div id='center_h' style='margin-left:580px;'><a href='/qa.php' >律氏知道</a>|<a href='/pro_guide.php'>专业指引</a></div>");
-					break;
-				case 10:
-					$("#cbx").html("<div id='center_h' style='margin-left:642px;'><a href='#' >专题培训</a>|<a href='#'>讲座</a>|<a href='#'>峰会</a></div>");
-					break;
-				case 11:
-					$("#cbx").html("<div id='center_h' style='margin-left:747px;'><a href='#' >申请加入</a></div>");
-					break;
-				case 12:
-					$("#cbx").html("<div id='center_h' style='margin-left:802px;'><a href='#' >律所订阅</a></div>");
-					break;
-			}
-		}
-        $(function(){
-			$('.nav').hover(function(){
-				var selected =$('.nav').index($(this));
-				show_submenu(selected);
-			});
-		});
-	</script>
 </div>
