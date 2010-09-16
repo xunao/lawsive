c687341fbed7998a0389bacdb158a5f18c05f31a
robbin <robbin@robbin-laptop.(none)>
2010-09-16 13:18:11 +0800

    robbin

---
+++ inc/home/left.php
@@ -0,0 +1,34 @@
+<div id="person_index_left">
+      		<div id="medal">
+      			<div id="close">X</div>
+      			<div id="top">
+      				勋章已升级
+      			</div>
+      			<div id="bottom">
+      				得的勋章推荐给粉丝
+      			</div>
+      		</div>
+      		<button>高级注册</button>
+      		<div class=title>我的律氏</div>
+      		<div class=titlebottom></div>
+      		<?php for($i=0;$i<10; $i++){ ?>
+      		<div class="content"><a href="">职业生涯(new)</a></div>
+      		<?php }?>
+      		<div class="system" style="margin-top:10px;"><a href="">在线会议系统</a></div>
+      		<div class="systemline"></div>
+      		<div class="system"><a href="">在线讲座系统</a></div>
+      		<div class=title>律氏服务</div>
+      		<div class=titlebottom></div>
+      		<div class="servertitle" style="margin-top:5px;"><a href="">律氏中文网</a></div>
+      		<?php for($i=0;$i<5;$i++){ ?>
+      		<div class="servercontent"><a href="">个人动态信息</a></div>
+      		<?php }?>
+      		<div class="servertitle"><a href="">律氏中文网</a></div>
+      		<?php for($i=0;$i<5;$i++){ ?>
+      		<div class="servercontent"><a href="">个人动态信息</a></div>
+      		<?php }?>
+      		<div class="servertitle"><a href="">律氏中文网</a></div>
+      		<?php for($i=0;$i<5;$i++){ ?>
+      		<div class="servercontent"><a href="">个人动态信息</a></div>
+      		<?php }?>
+      	 </div>
