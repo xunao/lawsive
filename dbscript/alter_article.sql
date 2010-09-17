9c795876e706a391e89249c0fb9cee107d297273
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-17 17:53:43 +0800

    xx

--- dbscript/alter_article.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `lawsive`.`article` MODIFY COLUMN `admin_user_id` INTEGER  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '发布者',
- MODIFY COLUMN `category` integer  CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '文章子分类';
-
