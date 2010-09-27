ef7e2771f2eee736d22b0c43bc33269b5a5ed62d
=375865795 <=375865795.com>
2010-09-27 13:17:47 +0800

    mood

--- project_lib/FriendNews.class.php
+++ project_lib/FriendNews.class.php
@@ -77,12 +77,12 @@ class FriendNews {
 				$this->content = mb_substr($comment->comment, 0,200,'utf-8');
 			break;
 			case 'mood':
-				$sql = "select a.id,a.admin_user_id,a.title,a.content,a.created_at,b.name from article a left join member b on a.admin_user_id = b.id where a.id ={$resource_id}";
+				$sql = "select a.id,a.u_id,a.content,a.created_at,b.name from mood a left join member b on a.u_id = b.id where a.id ={$resource_id}";
 				$result = $db->query($sql);
 				$result = $result[0];
 				if(!$result) return false;
-				$this->title = "<a href='/home/member.php?id={$result->admin_user_id}'>{$result->name}</a>　发布了一篇日记　";
-				$this->title .="<a href='/home/application/diary/show.php?id={$result->id}'>". mb_substr($result->title, 0,10,'utf-8') ."</a>";
+				$this->title = "<a href='/home/member.php?id={$result->u_id}'>{$result->name}</a>　发布了一个心情　";
+				$this->title .="<a href='/home/mood/show.php?id={$result->id}'>". mb_substr($result->content, 0,10,'utf-8') ."</a>";
 				$this->created_at = $result->created_at;
 				$this->content = mb_substr($result->content, 0,200,'utf-8');
 			break;
