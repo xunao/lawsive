7de02d4feca4e3fdcc181f0db359b74419770b58
=375865795 <=375865795.com>
2010-09-27 17:41:44 +0800

    mood

--- home/friend_news.post.php
+++ home/friend_news.post.php
@@ -1,15 +1,29 @@
 <?php
 	include_once('../frame.php');
-	$db=get_db();
-	$user = User::current_user();
+//	$db=get_db();
+	//$user = User::current_user();
+	$user=member::current();
 	if(!$user) die('对不起，您的登录已过期！请重新登录！');
-	$select =$_POST['select'];
-	$type = $_POST['type'];
-	if(!in_array($type,array('all','oneword','diary','image'))){die('no such type!');}
-	
-    if($result){
-    for($i=0;$i<$num;$i++){
-    	
-    }
-    }
+
+    //$type = $_POST['type'];
+//	if(!in_array($type,array('all','oneword','diary','image'))){die('no such type!');}
+
    ?>
+ <?php 
+        $type=$_POST['type'];
+        $friend_news = $user->get_friend_news($type);
+        foreach ($friend_news as $friendnews){
+        ?>
+        <div class="context">
+        <div class="c_title">
+        <?php echo $friendnews->title?>
+        <div class="day"><?php echo $friendnews->created_at?></div>
+        </div>
+        <div class="cc">
+        <?php echo $friendnews->content;?>
+        </div>
+        <!-- <div class="comment"><a href="">发表评论</a></div>  -->
+        </div>
+ <?php }?>
+   
+   
--- javascript/home.js
+++ javascript/home.js
@@ -1,30 +1,38 @@
 $(function(){
-	$.post('friend_news.post.php',{'type':'all'},function(data){
-		$('#test').html(data);
-	});
+//	$.post('friend_news.post.php',{'type':'all'},function(data){
+//		$('#test').html(data);
+//	});
 	$('.lable').click(function(){
 		$('.lable').attr('class','lable');
 		$(this).attr('class','lable select');
-		var select = $('.lable').index();
+		var select = $('.lable').index($(this));
 		if(select == '0'){
-			$.post('lastest_news.post.php',{'type':'all'},function(data){
-				$('#test').html(data);
+			$.post('friend_news.post.php',{'type':'all'},function(data){
+				$('#b_contest').html(data);
 			});
 		}
 		if(select == '1'){
-					
+			$.post('friend_news.post.php',{'type':'photo'},function(data){
+				$('#b_contest').html(data);
+			});		
 		}
 		if(select == '2'){
-					
+			$.post('friend_news.post.php',{'type':'dairy'},function(data){
+				$('#b_contest').html(data);
+			});		
 		}
 		if(select == '3'){
-					
+			$.post('friend_news.post.php',{'type':'comment'},function(data){
+				$('#b_contest').html(data);
+			});		
 		}
 		if(select == '4'){
 			
 		}
 		if(select == '5'){
-					
+			$.post('friend_news.post.php',{'type':'mood'},function(data){
+				$('#b_contest').html(data);
+			});		
 		}
 		if(select == '6'){
 					
